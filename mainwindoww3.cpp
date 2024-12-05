74fa6f7bc74fb32afab38f8a439373f75fc89198
souhaMzoughi <souha.mzoughi@esprit.tn>
2024-12-05 23:36:18 +0100

    sss

---
+++ mainwindoww3.cpp
@@ -0,0 +1,6 @@
+#include "mainwindow3.h"
+
+mainwindow3::mainwindow3()
+{
+
+}
---
+++ mainwindoww3.h
@@ -0,0 +1,11 @@
+#ifndef MAINWINDOW3_H
+#define MAINWINDOW3_H
+
+
+class mainwindow3
+{
+public:
+    mainwindow3();
+};
+
+#endif // MAINWINDOW3_H
